eb6da87cf44ccde88f56d6467016c030ea3db1ca
Rayyan05 <75606877+Rayyan05@users.noreply.github.com>
2021-07-27 11:52:42 +0530

    Add files via upload

---
+++ Daily.js
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class Daily extends React.Component{
+    render(){
+        return(
+            <View style = {{flex:1}}>
+                <Text>Daily pic Screen</Text>
+            </View>
+        )
+    }
+}
---
+++ StarMapScreen.js
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class SpacecraftScreen extends React.Component{
+    render(){
+        return(
+            <View style = {{flex:1}}>
+                <Text>Star Map Screen</Text>
+            </View>
+        )
+    }
+}
